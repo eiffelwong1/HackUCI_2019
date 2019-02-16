ff12268f21b625bf74d47ac46f81e10f13b8c1bc
Sing Wong <eiffelynx@gmail.com>
2019-02-16 14:28:39 -0800

    uploading android Dev all

---
+++ app/src/main/java/com/example/hackuci2019/UserRegister.java
@@ -0,0 +1,20 @@
+package com.example.hackuci2019;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+public class UserRegister extends AppCompatActivity {
+
+    private EditText editText_title;
+    private EditText editText_URL;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_register);
+
+
+    }
+
+}
